3aba7e6a5c4a7a1a444d667557894c3d13ddabea
enso\bbqck <kukwonho@gmail.com>
2025-03-17 23:06:20 +0900

    챕터7 - 구조화된 동시성 1. 코루틴 스코프 직접 생성하기 2. 부모 자식 코루틴

---
+++ src/main/kotlin/chapter7/code/Code6.kt
@@ -0,0 +1,18 @@
+package org.example.chapter7.code
+
+import kotlinx.coroutines.*
+
+@OptIn(ExperimentalStdlibApi::class)
+fun main() {
+    val newScope = CoroutineScope(CoroutineName("MyCoroutine") + Dispatchers.IO)
+    newScope.launch(CoroutineName("LaunchCoroutine")) {
+        println(this.coroutineContext[CoroutineName])
+        println(this.coroutineContext[CoroutineDispatcher])
+
+        val launchJob = this.coroutineContext[Job]
+        val newScopeJob = newScope.coroutineContext[Job]
+        println("launchJob?.parent === newScopeJob >> ${launchJob?.parent === newScopeJob}")
+    }
+
+    Thread.sleep(1000L)
+}
